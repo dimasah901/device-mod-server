e8721824f8e9c1bca0b87b3b760ff60dfe8dfa06
dimasah901 <93038732+dimasah901@users.noreply.github.com>
2024-03-12 10:57:02 +0500

    Made a base for making LUA servers, using static websites and responding with 404 Not Found

--- server.js
+++ server.js
@@ -7,10 +7,37 @@
     const factory = new LuaFactory();
     const lua = await factory.createEngine();
 
-    lua.global.set('hello', (func) => func());
+    let servers = {};
+    let servers_ = [];
 
-    app.get('/', async (req, res) => {
-        res.send(await lua.doString(`return hello(function () return 'hello world' end)`));
+    lua.global.set('server', (options, callback) => {
+        if (servers_.indexOf(options.url) == -1) {
+    	    let server = {}
+    	    servers[options.url] = server
+            servers_.push(options.url)
+
+    	    callback({
+    	        url: options.url,
+    	        get: (path, callback) => { server[path] = callback },
+    	        db: {}
+    	    });
+    	}
+    });
+
+    let static_websites = [];
+
+
+    app.get('*', async (req, res) => {
+        if (servers_.indexOf(req.url) == -1) {
+			if (static_websites.indexOf(req.url) == -1) {
+				res.send('404 Not Found')
+            } else {
+                res.send('a static website')
+            }
+        } else {
+            res.send(servers[req.url][req.url]);
+        }
+
     })
 
     app.listen(8000)
